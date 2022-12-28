a74bad0c2dda6d8e0c62fd95cd33ab7c513ed3cf
Icen <105867625+icenspln@users.noreply.github.com>
2022-12-28 17:15:39 +0100

    finished CTA

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,23 @@
 import "./cta.css";
 
 function CTA() {
-  return <div>CTA</div>;
+  return (
+    <>
+      <div className="CAT section__margin section__padding">
+        <div className="CAT__call">
+          <small className="CAT__request">
+            Request Early Access to Get Started
+          </small>
+          <h1 className="CAT__heading">
+            Register today & start exploring the endless possiblities.
+          </h1>
+        </div>
+        <div className="CAT__action">
+          <button className="CAT__button">Get Started</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default CTA;
